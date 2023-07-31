23a39cf1689649889864d30a12993244208a3aa0
akfagi <139698529+akfagi@users.noreply.github.com>
2023-07-31 03:21:57 +0300

    Web_Task1
    المهمة الأولى فيه مسار الويب, الكود والازرار تم رفعها

---
+++ controll.js
@@ -0,0 +1,38 @@
+
+// Controll.js
+function moveForward() {
+ 
+}
+
+function moveBackward() {
+  
+}
+
+function moveLeft() {
+ 
+}
+
+function moveRight() {
+ 
+}
+
+function stop() {
+
+}
+
+function reset() {
+  
+}
+
+// Dataa.js
+function updateStatus(newStatus) {
+  const statusElement = document.getElementById('status');
+  statusElement.textContent = newStatus;
+}
+
+function updateBatteryLevel(newLevel) {
+  const batteryElement = document.getElementById('battery');
+  batteryElement.textContent = newLevel;
+}
+
+
